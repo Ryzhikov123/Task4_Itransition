0ebceff1de837fb51f7753bfc9a495b699915d34
Nikita <dangersoglad@gmail.com>
2022-04-13 17:22:15 +0300

    edit gitignore

---
+++ client/build/precache-manifest.de9385098220072d1c2582a1c7b66ed5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3bbba696a1588d12edd306a4d76d92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b36268c62c30a1869c1b",
+    "url": "/static/css/2.a389d3b1.chunk.css"
+  },
+  {
+    "revision": "1392a5b4a6dda623cc46",
+    "url": "/static/css/main.16c6520a.chunk.css"
+  },
+  {
+    "revision": "b36268c62c30a1869c1b",
+    "url": "/static/js/2.90968d47.chunk.js"
+  },
+  {
+    "revision": "74dcd8052a644434877c2eb155510a9e",
+    "url": "/static/js/2.90968d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1392a5b4a6dda623cc46",
+    "url": "/static/js/main.157c59ec.chunk.js"
+  },
+  {
+    "revision": "abb42bf77afb4a2a3885",
+    "url": "/static/js/runtime-main.ac662269.js"
+  }
+]);
